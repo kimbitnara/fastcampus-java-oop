1a1aea5a4c329a9b71474a7e8eff09b4e341e4f8
nara <nara8042@gmail.com>
2023-01-21 18:34:12 +0900

    적당히 테스트
    ㅇㅇ

---
+++ fastcampus-java-oop/src/main/java/com/fastcampus/javaoop/logic/BubbleSort.java
@@ -0,0 +1,9 @@
+package com.fastcampus.javaoop.logic;
+
+import java.util.List;
+
+public class BubbleSort <T extends Comparable<T>> {
+    public List<T> sort(List<T> list){
+        return null;
+    }
+}
